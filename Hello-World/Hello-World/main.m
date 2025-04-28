ea29b7d5479704524449534fc07a20c1c201625b
Dillon Teakell <dillonteakell@Dillons-MacBook-Pro.local>
2025-04-28 11:21:31 -0400

    Added Hello-World Program

---
+++ Hello-World/Hello-World/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  Hello-World
+//
+//  Created by Dillon Teakell on 4/28/25.
+//
+// Print 'Hello, World!' to the console
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    
+    // Creates a reference-counted memory management system
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    
+    NSLog(@"Hello, World!");
+    
+    // Decrements the reference counter if there are multiple instances, deallocates if 1.
+    [pool drain];
+    
+    return 0;
+    
+}
